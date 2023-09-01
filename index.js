eaeccc9ec4f308778c2b3c6e89ea94bb350a39c0
Chidubem-Ifedigbo <chidubemifedigbo6@gmail.com>
2023-09-01 17:52:28 +0100

    added transition to mobile navigation

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 
 let mobile = document.querySelector('.mobile-navigation')
-// let header = document.querySelector('.header');
-mobile.style.left = '150%';
 let closeIcon = document.querySelector('.fa-xmark')
 let hamburgerIcon = document.querySelector('.fa-bars');
-hamburgerIcon.addEventListener('click', function appear(){
-   mobile.style.left = '30%'
+let ul = document.querySelector('.top-nav-ul');
+mobile.style.width = '0';
+let hidden = () =>{
+    ul.style.visibility = 'hidden';
+    closeIcon.style.visibility = 'hidden';
+}
+hidden();
+
+hamburgerIcon.addEventListener('click', function appear(){  
+    mobile.style.visibility = 'visible';
+    mobile.style.width = '70%';
+    closeIcon.style.visibility = 'visible';
+    setTimeout(() =>{
+        ul.style.visibility = 'visible'; 
+    } ,1500);
+    hamburgerIcon.style.visibility = 'hidden';
 })
+
 closeIcon.addEventListener('click', () => {
-        mobile.style.left = '150%';
-    })
-
-// header.addEventListener('click', () => {
-//          mobile.style.left = '150%';
-// })
-
- 
+    mobile.style.width = '0';
+    hamburgerIcon.style.visibility = 'visible';
+    hidden()
+})
