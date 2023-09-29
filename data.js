c851e6a62a392f4a5f0d3ff513d7832fce13e7ba
Chidubem-Ifedigbo <chidubemifedigbo6@gmail.com>
2023-09-29 12:55:47 +0100

    added data.js and populated the services section from the index.js using js map

---
+++ data.js
@@ -0,0 +1,25 @@
+
+
+export const services = [
+  {
+    image:'./img/Academy.png',
+    title: 'Academy',
+    desc: 'Gain access to exclusive expert-led courses that are student and hand-on driven. Master your craft and stand out globally with our Master of Practice Certification..',
+    button: true
+  },
+  {
+    image:'./img/Marketplace.png',
+    title: 'Market Place',
+    desc: 'Shop for quality and reliable innovative products, ideas, project resources, gadgets, and educational materials.',
+  },
+  {
+    image:'./img/ICT-Solutions.png',
+    title: 'ICT Soution',
+    desc: 'Find out and bargain for best deals of the outstanding services we offer.We have an excellent network of professionals in teams to meet your needs..',
+  },
+  {
+    image:'./img/staff-product.png',
+    title: 'Staff Product',
+    desc: 'See outstanding portfolio of our graduates for your hire and partnership as the case may be. We know and trust them enough to recommend.',
+  }
+]
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+import { services } from "./data.js";
 
 let mobile = document.querySelector('.mobile-navigation')
 let closeIcon = document.querySelector('.fa-xmark')
 let hamburgerIcon = document.querySelector('.fa-bars');
 let ul = document.querySelector('.top-nav-ul');
+let servicesSection = document.getElementById('services');
+let filterSearch = document.querySelector('.filter')
+let filteredArray = []
+
+
 mobile.style.width = '0';
 let hidden = () =>{
     ul.style.visibility = 'hidden';
@@ -25,3 +31,89 @@ closeIcon.addEventListener('click', () => {
     hamburgerIcon.style.visibility = 'visible';
     hidden()
 })
+// console.log(services)
+let actualService = services;
+
+
+
+// 29/09/2023
+
+//run the map function to populate the service section of the HTML
+let serviceMap = () => {
+    
+    actualService.map((service) => {
+
+        let div = document.createElement('div');
+
+        let img = document.createElement('img');
+        img.setAttribute('src',service.image)
+        
+        let h3 = document.createElement('h3');
+        h3.setAttribute('class','service-title');
+        h3.innerText = service.title;
+        
+        let para = document.createElement('p');
+        para.setAttribute('class','service-description');
+        para.innerText = service.desc;
+        
+        let button1 = document.createElement('button');
+        button1.setAttribute('class','learn-more');
+        button1.innerText = 'Learn More';
+        
+        let button2 = document.createElement('button');
+        button2.setAttribute('class','apply-now');
+        button2.innerHTML = 'Apply Now';
+        
+        let button3 = document.createElement('button');
+        button3.setAttribute('class','comming-soon');
+        button3.innerHTML = 'Comming Soon';
+        
+        div.append(img,h3,para);
+        
+        service.button ? div.append(button1,button2) : div.appendChild(button3);
+        
+        // if(service.button){
+            //     div.append(button1,button2)
+            // }else{
+                //     div.appendChild(button3) 
+                // }
+                
+                servicesSection.appendChild(div)
+                // console.log(div);
+                
+    });
+           
+};
+
+serviceMap();
+
+//array.push
+
+
+// let arr = [2,4,5,6,7,8,9]
+// arr.push(11)
+// console.log(arr);
+// arr.pop()
+// console.log(arr)
+// let splicedArray = arr.splice(2,3)
+// console.log(splicedArray);
+// console.log(arr)
+
+// let mappedArray = arr.map((num)=> {
+//   return num*2
+// })
+// // console.log(mappedArray)
+// let filtered = arr.filter((num) => {
+//     return num%2 !== 0;
+// })
+
+// console.log(filtered);
+
+// run the filter here
+// filterSearch.addEventListener('keydown',()=>{
+//     for(let i = 0; i < actualService.length; i++){
+//         if(actualService[i].title.includes(filterSearch.value)){
+//             filteredArray.push(actualService[i])
+//         }
+//     }
+// })
